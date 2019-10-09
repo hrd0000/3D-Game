f5eb70f85d27df5ab3b6ec8cca90b7dbf5095354
hrd0000 <huord@mail2.sysu.edu.cn>
2019-10-09 16:09:18 +0800

    fix

---
+++ Assignment5/Scripts/SceneController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UFO.com {
+    public interface IUserAction {
+        void launchUFO();
+        void hitUFO(Vector3 mousePos);
+    }
+
+    public interface IGameStatusOp {
+        int getRoundNum();
+        void addScore();
+        void subScore();
+    }
+    
+    public class SceneController : System.Object, IUserAction, IGameStatusOp {
+        private static SceneController instance;    //单例模式
+        private UFOController myUFOCtrl;          //飞碟管理
+        private StatusController myStatusCtrl;        //记分员
+            
+        public static SceneController getInstance() {
+            if (instance == null) instance = new SceneController();
+            return instance;
+        }
+
+        internal void setUFOController(UFOController ufoCtrl) {
+            if (myUFOCtrl == null) myUFOCtrl = ufoCtrl;
+        }
+
+        internal int getTrailNum() {
+            return myStatusCtrl.getTrialNum();
+        }
+
+        internal void setStatusController(StatusController gameStatus) {
+            if (myStatusCtrl == null) myStatusCtrl = gameStatus;
+;        }
+
+        public void addScore() {
+            myStatusCtrl.addScore();
+        }
+
+        public int getRoundNum() {
+            return myStatusCtrl.getRoundNum();
+        }
+
+        public void subScore() {
+            myStatusCtrl.subScore();
+        }
+
+        public void launchUFO() {
+            myStatusCtrl.resetScore();
+            myUFOCtrl.launchUFO();
+        }
+
+        public void hitUFO(Vector3 mousePos) {
+            myUFOCtrl.hitUFO(mousePos);
+        }
+    }
+}
+
---
+++ Assignment5/Scripts/UFOFactoryBC.cs
@@ -0,0 +1,66 @@
+﻿using UFO.com;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFOFactory : System.Object {
+    private static UFOFactory instance;                                        //单实例
+    private List<GameObject> usedUFOList = new List<GameObject>();            //使用列表
+    private List<GameObject> freeUFOList = new List<GameObject>();           //空闲列表
+    private GameObject UFOItem;
+
+    public static UFOFactory getInstance() {
+        if (instance == null) instance = new UFOFactory();
+        return instance;
+    }
+
+    public GameObject getUFO() {
+        if(freeUFOList.Count == 0) {
+            GameObject newUFO = Camera.Instantiate(UFOItem);
+            usedUFOList.Add(newUFO);
+            return newUFO;
+        }
+        else {
+            GameObject oldUFO = freeUFOList[0];
+            freeUFOList.RemoveAt(0);
+            oldUFO.SetActive(true);
+            usedUFOList.Add(oldUFO);
+            return oldUFO;
+        }
+    }
+
+    public void detectLandingUFOs() {
+        for(int i = 0; i < usedUFOList.Count; i++) {
+            if(usedUFOList[i].transform.position.y <= -8) {
+                usedUFOList[i].GetComponent<Rigidbody>().velocity = Vector3.zero;
+                usedUFOList[i].SetActive(false);
+                freeUFOList.Add(usedUFOList[i]);
+                usedUFOList.Remove(usedUFOList[i]);
+                i--;
+            }
+        }
+    }
+
+    public void RecyclingUFO(GameObject UFOObject) {
+        UFOObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        UFOObject.SetActive(false);
+        freeUFOList.Add(UFOObject);
+        usedUFOList.Remove(UFOObject);
+    }
+    
+    public bool isLaunching() {
+        return (usedUFOList.Count > 0);
+    }
+
+    public void initItems(GameObject ufoItem) {
+        UFOItem = ufoItem;
+    }
+}
+
+public class UFOFactoryBC: MonoBehaviour {
+    public GameObject ufoItem;
+
+    private void Awake() {
+        UFOFactory.getInstance().initItems(ufoItem);
+    }
+}
