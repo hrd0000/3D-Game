770bfe2c1bcd93d604af46f4397257324f10c27c
hrd0000 <huord@mail2.sysu.edu.cn>
2019-11-09 22:40:15 +0800

    Assignment8

---
+++ Assignment8/halo/Assets/init.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class init : MonoBehaviour {
+    public ParticleSystem particleSystem;
+    public Camera camera;
+    public int particleNum = 10000; // 粒子数目
+    public float minRadius = 5.0f;  // 光环最小半径
+    public float maxRadius = 10.0f; // 光环最大半径
+
+    private ParticleSystem.Particle[] particles;
+    private float[] particleAngle;  // 粒子角度
+    private float[] particleR;  // 粒子半径
+    private int speedLevel = 5; // 粒子旋转速度水平
+    private float particleSpeed = 0.1f;  // 粒子旋转速度
+
+    private Ray ray;
+    private RaycastHit hit;
+
+    private float[] before;   
+    private float[] after;   
+    private float shrinkSpeed = 2f;
+    private bool ischange = false;
+
+	void Start () {
+        particleAngle = new float[particleNum];
+        particleR = new float[particleNum];
+        before = new float[particleNum];
+        after = new float[particleNum];
+        particles = new ParticleSystem.Particle[particleNum];
+
+        particleSystem.maxParticles = particleNum;
+        particleSystem.Emit(particleNum);
+        particleSystem.GetParticles(particles);
+
+        Ndistribution nd = new Ndistribution();
+
+        for (int i = 0; i < particleNum; i++) {
+            float r = (float)nd.getNormalDistribution((minRadius+maxRadius)*0.5f, 1);
+            float angle = UnityEngine.Random.Range(0.0f, 360.0f);
+            particleAngle[i] = angle;
+            particleR[i] = r;
+
+            before[i] = r;
+            after[i] = 0.1f * r;
+            
+            if (after[i] < minRadius * 1.1f) {
+                float midRadius = minRadius * 2f;
+                after[i] = UnityEngine.Random.Range(UnityEngine.Random.Range(minRadius, midRadius), (minRadius * 1.1f));
+            }
+        }
+	}
+
+	void Update () {
+		for(int i = 0; i < particleNum; i++) {
+            if (ischange) {
+                if(particleR[i] > after[i]) {
+                    particleR[i] -= shrinkSpeed * (particleR[i] / after[i]) * Time.deltaTime;
+                }
+            } else {
+                if (particleR[i] < before[i]) {
+                    particleR[i] += shrinkSpeed * (before[i] / particleR[i]) * Time.deltaTime;
+                } else if (particleR[i] > before[i]) {
+                    particleR[i] = before[i];
+                }
+            }
+
+            if (i % 2 == 0) {
+                particleAngle[i] += (i % speedLevel + 1) * particleSpeed;  
+            } else {
+                particleAngle[i] -= (i % speedLevel + 1) * particleSpeed;  
+            }  
+
+            particleAngle[i] = particleAngle[i] % 360;
+            float rad = particleAngle[i] / 180 * Mathf.PI;  
+
+            particles[i].position = new Vector3(particleR[i] * Mathf.Cos(rad), particleR[i] * Mathf.Sin(rad), 0f);  
+        }
+        particleSystem.SetParticles(particles, particleNum);  
+        ray = camera.ScreenPointToRay(Input.mousePosition);  
+        if (Physics.Raycast(ray, out hit) && hit.collider.gameObject.tag == "button") ischange = true;  
+        else ischange = false;  
+	}
+}
+
+class Ndistribution {
+    System.Random rand = new System.Random();
+    public double getNormalDistribution(double mean, double stdDev) {
+        double u1 = 1.0 - rand.NextDouble(); //uniform(0,1] random doubles
+        double u2 = 1.0 - rand.NextDouble();
+        double randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) *
+                     Math.Sin(2.0 * Math.PI * u2); //random normal(0,1)
+        double randNormal = mean + stdDev * randStdNormal; //random normal(mean,stdDev^2)
+        return randNormal;
+    }
+}
--- README.md
+++ README.md
@@ -2,22 +2,15 @@
 
 演示视频：[单人五子棋](https://www.bilibili.com/video/av67610807)
 
-
-
 演示视频：[模拟太阳系&牧师与魔鬼](https://www.bilibili.com/video/av68569632)
-
-
-
-
 
 演示视频：[牧师与魔鬼 动作分离版](https://www.bilibili.com/video/av69333767)
 
 演示视频：[hit UFO](https://www.bilibili.com/video/av70662982)
 
-
-
 演示视频：[改进飞碟](https://www.bilibili.com/video/av71642907)
 
-
-
 演示视频：[简单巡逻兵](https://www.bilibili.com/video/av73555354/)
+
+演示视频：[粒子光环]()
+
